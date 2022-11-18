7e84bef1bfe28c73671f2f5169d92dbfdf5e5463
Ruby An <ruby@penguin>
2022-11-18 09:06:16 -0500

    troubleshooting carbon gain

---
+++ experiment/experimental_design.R
@@ -0,0 +1,17 @@
+## Created 20 August 2022
+## Author: RubyAn
+
+## Packages
+load("tidyverse")
+
+
+blocks <- c(1:10)
+plots <- c(1:8)
+
+treatments <- c("C", "W", "N", "S", "W+N", "W+S", "N+S", "W+N+S")
+
+
+plot_id <- c(1:80)
+
+
+set.seed(1)
