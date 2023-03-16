e113fb3921b3060bacb05d3e76b484cd1d3d84c6
moonshine <ruby@dynamic-oit-vapornet100-10-9-239-5.princeton.edu>
2023-03-16 12:39:01 -0400

    taking out experimental design

--- code/Models.R
+++ code/Models.R
@@ -10,13 +10,38 @@ one_sp_one_resource <- function(t, y, p) { # one sp/one resource consumer resour
   #                 L (plant loss rate) 
   #                 h (handling time, or saturating, set h = 0 to get linear growth function)
   
-  B = y[1]
-  R = y[2]
+  R = y[1]
+  B = y[2]
   
   ## outputs: B (biomass), R (resource level) 
   with(as.list(p),{
-    dR.dt = r - g*B*R;
-    dB.dt = f*g*R*B/(1 + h*g*R) - L*B; 
-    return(list(c(dB.dt,  dR.dt)));
+    dR.dt = r - g*R/(1+g*h*R)*B;
+    dB.dt = f*g*R*B/(1 + g*h*R) - L*B; 
+    return(list(c(dR.dt,  dB.dt)));
+  })
+}
+
+two_sp_one_resource <- function(t, y, p) { # one sp/one resource consumer resource model w/handling time
+  ## inputs 
+  # t: time
+  # y: initial conditions [R, B1, B2] 
+  # p: parameters : r (resource supply rate), 
+  #                 g_n (plant growth rate), 
+  #                 f_n (resource conversion efficiency)
+  #                 L_n (plant loss rate) 
+  #                 h_n (handling time, or saturating, set h = 0 to get linear growth function)
+  
+  R = y[1]
+  B1 = y[2]
+  B2 = y[3]
+  
+  
+  ## outputs: B (biomass), R (resource level) 
+  with(as.list(p),{
+    dR.dt = r - g1*R*B1/(1 + h1*g1*R) - g2*R*B2/(1 + h2*g2*R)
+    dB1.dt = f1*g1*R*B1/(1 + h1*g1*R) - L1*B1; 
+    dB2.dt = f2*g2*R*B2/(1 + h2*g2*R) - L2*B2; 
+    
+    return(list(c(dR.dt, dB1.dt, dB2.dt )));
   })
 }
---
+++ otc_cals.R
@@ -0,0 +1,67 @@
+## OTC calculations 
+library(tidyverse)
+
+# inputs - set the large dimensions
+x = 110 # long width
+z = 50 # height
+phi = 60*2*pi/360 # panel angle 
+
+
+# derived dimensions
+a = x/2
+y = sqrt(3)*a
+theta = atan(y/(a*cos(phi)))
+d = z/sin(phi)
+c = d/sin(theta)
+e = a*z/y * (cos(theta)/sin(theta))
+b = a - 2*e
+
+
+# print
+x
+y
+z
+phi
+
+a
+b
+c
+d
+theta*360/(2*pi)
+
+## dimensions to cut
+long = a+4
+short = b+4
+height = d
+
+long
+short
+height
+
+#3 area
+print(str_c((long + short + e)*60, " cm2"))
+
+# 7080 cm2 per 
+
+## cm to inches: 1 inch = 2.54 cm
+
+### Set Panel Dimensions ----- 
+
+a = 55
+phi = 60*2*pi/360 # panel angle 
+d = 50
+
+x = 2*a
+y = sqrt(3)/2*x
+z = d*sin(theta)
+theta
+
+
+e = d/tan(theta)
+b = a-2*e
+c = d/sin(theta)
+
+
+
+
+
